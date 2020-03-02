d070e640bf2e20a06248f21afac27de3309d8c12
OskariAuvinen <55886851+OskariAuvinen@users.noreply.github.com>
2020-03-02 14:57:48 +0200

    heroku2

---
+++ public/precache-manifest.0be01c0b463317c03001da87034eefaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3afbbcc4488fc6c2f3ab0a45d4f448b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0549eaa92b6205bba8a2",
+    "url": "/static/css/main.ed149b1a.chunk.css"
+  },
+  {
+    "revision": "745c3b7950e5ce5bfc9e",
+    "url": "/static/js/2.3dc84e10.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3dc84e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0549eaa92b6205bba8a2",
+    "url": "/static/js/main.27246e6a.chunk.js"
+  },
+  {
+    "revision": "6ab2a229bd22b9911c0f",
+    "url": "/static/js/runtime-main.eba4b630.js"
+  }
+]);
